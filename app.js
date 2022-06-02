7f221448c57c9b82853b08af14e7554f85392178
Abdus Samiul Basir <samiulprangon2013@gmail.com>
2022-06-02 11:58:50 +0600

    write comment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// all the variables for the project.
 const changeLevelHeadin = "You win! Start next level";
 const levelHeading = `Level`;
 const gameOverHeading = "Game Over Please press a key to try again";
@@ -10,6 +11,8 @@ let validityCheckIndex = 0;
 let gameStatus = false;
 
 
+
+// this takes a function and runs after ms amount of time.
 function sleep (func, ms) {
     return new Promise(resolve => setTimeout(() => {
         resolve(func())
@@ -17,6 +20,7 @@ function sleep (func, ms) {
 };
 
 
+// it finds a new color from the buttonColors and insurt it into the gamePattern resulting a pattern for one round. 
 const nextSequence = () => {
     const randomNumber = Math.round(Math.random() * 3);
     const randomChosenColor = buttonColors[randomNumber];
@@ -24,12 +28,14 @@ const nextSequence = () => {
 };
 
 
+// takes a name of a file and plays it from the sounds folder if its there.
 const btnSound = (divId) => {
     const audio = new Audio(`/sounds/${divId}.mp3`);
     audio.play();
 };
 
 
+// button animation while showing the pattern.
 const btnAnimation = (divId) => {
     btnSound(divId);
     $(`#${divId}`).fadeOut().fadeIn();
@@ -39,7 +45,9 @@ const btnAnimation = (divId) => {
 const wrongAnswerAnimation = () => {
     const body = $("body");
     btnSound("wrong");
+    // game-over css property applys to the body.
     body.addClass("game-over");
+    // removes the game-over css after 100ms to give a sort of animation.
     setTimeout(() => {
         body.removeClass("game-over");
     }, 100);
@@ -47,26 +55,32 @@ const wrongAnswerAnimation = () => {
 
 
 const loadGamePattern = async () => {
+    // pointer-events prevents any clicks from happenning.
+    // it is necessary for the program so that the game pattern showcase runs smoothly without interference.  
     button.css("pointer-events", "none");
+    // creates a pattern depanding on the lavel.
     for (let i = 0; i < level; i++) {
         nextSequence();
     }
-
+    // shows the gamePattern chronologically.
     for (let pattern of gamePattern) {
         await sleep(() => btnAnimation(pattern), 1000);
     }
     gameStatus = true;
+    // removes the css to get user inputs.
     button.css("pointer-events", "auto");
 };
 
 
 const setHeading = (text, level) => {
+    // level is a optional argument it is to show the level of the game
     level = level || 0;
     const levelTitle = document.querySelector("#level-title");
     level === 0 ? levelTitle.innerHTML = text : levelTitle.innerHTML = `${text} ${level}`;
 };
 
 
+// creates animation with the pressed class css property.
 const keyPressAnimation = (clickedBtn) => {
     clickedBtn.addClass("pressed");
     btnSound(clickedBtn.attr('id'));
@@ -76,6 +90,7 @@ const keyPressAnimation = (clickedBtn) => {
 };
 
 
+// it resets the value that are common for the game over and completed level.
 const resetGameValue = () => {
     gamePattern.length = 0;
     validityCheckIndex = 0;
@@ -103,25 +118,34 @@ const resetLevel = () => level = startingLevel;
 
 
 button.click(function (e) {
+    // clickedBtn catches the this event.
     const clickedBtn = $(this);
+
     keyPressAnimation(clickedBtn);
+
+    
     if (gameStatus) {
-        if (validityCheckIndex < gamePattern.length - 1 ) {
+        if (validityCheckIndex < gamePattern.length - 1) {
+            // checks if the input matches the sequence.
             if (clickedBtn.attr('id') === gamePattern[validityCheckIndex]) {
                 validityCheckIndex++;
             } else {
                 gameOver();
             }
+        // this is an exception case the if statement corrosponding to that passes to the else value if the starting level is 1.
         } else if (gamePattern.length === 1) {
+            // as it contains only one pattern so it chacks the value if its true goes to the next level.
             if (clickedBtn.attr('id') === gamePattern[validityCheckIndex]) {
                 nextLevel();
             } else {
                 gameOver(); 
             }
+        // if passes every check without entering into gameOver then moves to next round.
         } else {
             nextLevel();   
         }
     } else {
+        // if the gameStatus is false means its time to make a new gamePattern.
         setHeading(levelHeading, level);
         loadGamePattern();
     }
